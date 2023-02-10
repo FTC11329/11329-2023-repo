98ce60e4197a1f41e167b27b67c5ebf95c87dde2
Kennan <kennanhunter5@gmail.com>
2023-02-10 15:59:06 -0500

    add light teleop for judging

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/LED.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/LED.java
@@ -4,7 +4,6 @@ import com.fizzyapple12.javadi.DiContainer;
 import com.fizzyapple12.javadi.DiInterfaces;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.robot.Robot;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
@@ -37,22 +36,19 @@ public class LED implements DiInterfaces.IInitializable, DiInterfaces.IDisposabl
     }
 
     // note: bad things happen, if this returns more than 1
-    private double getPower(double seconds) {
+    public static double getPower(double seconds, LEDEffect ledEffect) {
         double power = 0;
 
-        if (effect == LEDEffect.BREATHING) {
+        if (ledEffect == LEDEffect.BREATHING) {
             double sinInput = (Math.PI * seconds) / 2;
 
             power = Math.abs((Math.sin(sinInput)));
-        }
-        else if (effect == LEDEffect.CONSTANT) {
+        } else if (ledEffect == LEDEffect.CONSTANT) {
             power = RobotConfig.LED.CONSTANT_POWER;
-        }
-        else if(effect == LEDEffect.MORSE){
-            power = RobotConfig.LED.Custom.ICEMORSE[((int) (seconds*2))%RobotConfig.LED.Custom.ICEMORSE.length];
-        }
-        else if(effect == LEDEffect.FLASH){
-            power = RobotConfig.LED.Custom.FLASH[((int) (seconds*2))%RobotConfig.LED.Custom.FLASH.length];
+        } else if (ledEffect == LEDEffect.MORSE) {
+            power = RobotConfig.LED.Custom.ICEMORSE[((int) (seconds * 2)) % RobotConfig.LED.Custom.ICEMORSE.length];
+        } else if (ledEffect == LEDEffect.FLASH) {
+            power = RobotConfig.LED.Custom.FLASH[((int) (seconds * 2)) % RobotConfig.LED.Custom.FLASH.length];
         }
 
         return Range.clip(power, 0, 1);
@@ -60,7 +56,7 @@ public class LED implements DiInterfaces.IInitializable, DiInterfaces.IDisposabl
 
     @Override
     public void onTick() {
-        LED.setPower(getPower(elapsedTime.seconds()) * RobotConfig.LED.MaxLightPower);
+        LED.setPower(getPower(elapsedTime.seconds(), effect) * RobotConfig.LED.MaxLightPower);
     }
 
     @Override
